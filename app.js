419c5397ab00124e1e2c8b9349fec53c45034c76
zhuo.liu <zhuo.liu@qunar.com>
2018-08-26 23:28:30 +0800

    feat: es6

---
+++ app.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill'
+let func = ()=> {}
+const NUM = 45
+let arr = [1, 2, 3]
+let arr2 = arr.map(item => item * 2)
+arr.includes(3);
+console.log(`new Set(arr2)${new Set(arr2)}`)
+function* gen() {
+
+}
