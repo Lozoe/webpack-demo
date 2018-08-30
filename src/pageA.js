ec9ad84261b66286a8040445ea256085a9f2090b
zhuo.liu <zhuo.liu@qunar.com>
2018-08-30 14:00:26 +0800

    feat: code split

---
+++ src/pageA.js
@@ -0,0 +1,46 @@
+
+require.include('./moduleA')
+if(page === 'subPageA') {
+    require.ensure(['./subPageA'], function() {
+        var subPageA = require('./subPageA')
+    }, 'subPageA')
+} else if(page === 'subPageB') {
+    require.ensure(['./subPageB'], function() {
+        var subPageB = require('./subPageB')
+    }, 'subPageB')
+}
+
+require.ensure(['lodash'], function() {
+    var _ = require('lodash')
+    _.join([1, 2], 3)
+}, 'vendor')
+
+export default 'pageA'
+
+// import './subPageA'
+// import './subPageB'
+
+// require.ensure(['lodash'], function() {
+//     var _ = require('lodash')
+//     _.join([1, 2], 3)
+// }, 'vendor')
+
+// export default 'pageA'
+
+// import * as _ from 'lodash'
+// var page = 'subpageA'
+
+// if (page === 'subpageA') {
+//     import(/* webpackChunkName:'subpageA' */'./subPageA')
+//         .then(function(subPageA) {
+//             console.log(subPageA)
+//         })
+// } else if (page === 'subPageB') {
+//     import(/* webpackChunkName:'subpageB' */'./subPageB')
+//         .then(function(subPageB) {
+//             console.log(subPageB)
+//         })
+// }
+
+
+// export default 'pageA'
