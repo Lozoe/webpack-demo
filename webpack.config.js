9a331b97c008357c5187d73e4ac858fb6d3c13d6
zhuo.liu <zhuo.liu@qunar.com>
2018-09-08 22:58:43 +0800

    feat: sprites retina

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ var PurifyCSS = require('purifycss-webpack')
 var glob = require('glob-all')
 var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
 
+var extractLess = new ExtractTextWebpackPlugin({
+    filename: 'css/[name].bundle.css',
+})
+
 module.exports = {
     entry: {
         app: './src/app.js'
@@ -11,7 +15,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        // publicPath: 'dist/',
+        publicPath: 'dist/',
         filename: '[name].bundle.js',
         chunkFilename: '[name].bundle.js'
     },
@@ -20,20 +24,44 @@ module.exports = {
         rules: [
             {
                 test: /\.less$/,
-                use: ExtractTextWebpackPlugin.extract({
-                    fallback: {
-                        loader: 'style-loader',
-                        options: { singleton: true }
-                    },
-                    use: [
-                        {
-                            loader: 'css-loader',
+                use: extractLess.extract(
+                    {
+                        fallback: {
+                            loader: 'style-loader',
+                            options: {
+                                singleton: true,
+                                // transform: './css.transform.js'
+                            }
                         },
-                        {
-                            loader: 'less-loader'
-                        }
-                    ]
-                })
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    importLoaders: 2
+                                    // minimize: true,
+                                    // modules: true,
+                                    // localIdentName: '[path][name]_[local]_[hash:base64:5]'
+                                }
+                                // loader: 'file-loader'
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    ident: 'postcss',
+                                    plugins: [
+                                        require('postcss-sprites')({
+                                            spritePath: 'dist/assets/imgs/sprites',
+                                            retina: true
+                                        }),    
+                                        require('postcss-cssnext')()
+                                    ]
+                                }
+                            },
+                            {
+                                loader: 'less-loader'
+                            }
+                        ]
+                    })
             },
             {
                 test: /\.(png|jpg|jpeg|gif)$/,
@@ -52,20 +80,21 @@ module.exports = {
                         loader: 'url-loader',
                         options: {
                             // name: '[name]-[hash:5].[ext]',
+                            name: '[name].min.[ext]',
                             limit: 5000,
                             publicPath: '',
-                            outputPath: 'dist/',
+                            outputPath: 'dist/', // output publicPath
                             useRelativePath: true
                         }
+                    },
+                    {
+                        loader: 'img-loader',
+                        options: {
+                            pngquant: {
+                                quality: 80
+                            }
+                        }
                     }
-                    // {
-                    //     loader: 'img-loader',
-                    //     options: {
-                    //         pngquant: {
-                    //             quality: 80
-                    //         }
-                    //     }
-                    // }
                 ]
             },
         ]
