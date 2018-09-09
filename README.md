2df91a08508406374ebd9afe3c34b8fcb1792f96
zhuo.liu <zhuo.liu@qunar.com>
2018-09-09 14:48:02 +0800

    feat: long cache

--- README.md
+++ README.md
@@ -1,75 +1,28 @@
-# webpack-html-demo
+# webpack-long cache-demo
 
-### 方法
+### 长缓存优化
 ```javascript
-自动生成html
-场景优化
+<!--一、改变app,vendor变化-->
+提取vendor
+hash->chunkHash
+提取Webpack runtime
+<!--二、引入新模块  模块顺序变化， vendor hash变化-->
+思路：给chunks & module模块使用name标识 而非ID
+NamedChunkedPlugin
+NamedModulesPlugin
+<!--三、动态引入新模块 ，vendor hash变化-->
+定义动态模块的chunkName
 
+import(/* webpackChunkName: 'async' */'./async').then(function(a) {
+    console.log(a)
+}) 
 ```
-### 自动生成html
-HtmlWebpackPlugin
-options
-    template （自己加loader）
-    filename
-    minify
-    chunks 指定entrychunk excludeChunks
-    inject
-```
-npm i html-webpack-plugin --save-dev
-new Webpack.ProvidePlugin({
-    $: 'jquery'
-})
+### 总结
 
-//如果不是npm 模块，而是本地 可是使用alias
-resolve: {
-    alias: {
-        jquery$: path.resolve(__dirname, 'src/libs/jquery.min.js') //$ 确定只是把jquery关键字解析到某一个目录的文件下，不是解析到某一个目录 确切匹配
-    }
-},
-```
+- 独立打包vendor 分离第三方插件和业务代码 完全独立打包vendor
+- 抽出manifest(webpack runtime webpack打包时runtime代码会变化，为了让runtime的变化不影响vendor， 可以抽取 并且直接inline到html)  
+- NamedChunkedPlugin
+- NamedModulesPlugin (给定chunk和module不再是由webpack随机分配的从[0]-...的id,而是确切的不会随代码变化或者顺序变化而变化的id)
+- 动态载入模块给定模块名称
 
-### imports-loader
-```
-npm i imports-loader --save-dev
-```
-
-### 引入图片
-```js
-html-loader 指定html参数有哪些参数是交给webpack处理的
-options
-   attrs: [img:src]
-
-npm i html-loader --save-dev
-
-<!--html-->
-<img src="src/assets/imgs/elephant.png" data-src="src/assets/imgs/elephant.png" alt="">
-
-{
-    test:/\.html$/,
-    use: [
-        {
-            loader: 'html-loader',
-            options: {
-                attrs: ['img:src', 'img:data-src']
-            }
-        }
-    ]
-}
-
-或者：
-<img src="assets/imgs/elephant.min.png" data-src="src/assets/imgs/elephant.png"/>
-
-```
-
-### 优化
-
-```js
-提前载入webpack加载代码 减少浏览器请求
-inline-webpack-manifest-plugin
-html-webpack-inline-chunk-plugin
-
-npm i html-webpack-inline-chunk-plugin --save-dev
-npm i babel-core babel-loader babel-preset-env --save-dev
-
-```
-
+通过以上方法保证在打包第三方代码或者公用代码时，尽可能保证使用长缓存，保证版本号不再变化，保证用户浏览网站时尽可能使用缓存，加快访问速度和性能。
--- webpack.config.js
+++ webpack.config.js
@@ -1,208 +1,29 @@
 var path = require('path')
 var Webpack = require('webpack')
-var PurifyCSS = require('purifycss-webpack')
-var glob = require('glob-all')
-var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var HtmlInlinkChunkPlugin = require('html-webpack-inline-chunk-plugin')
-
-// var extractLess = new ExtractTextWebpackPlugin({
-//     // filename: 'css/[name]-bundle-[hash:5].css',
-//     filename: 'css/[name].bundle.css',
-// })
-
 module.exports = {
     entry: {
-        app: './src/app.js'
+        main: './src/foo',
+        vendor: ['react']
     },
 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        // publicPath: 'dist/',
-        // publicPath: '/',
-        filename: '[name]-bundle-[hash:5].js',
-        // filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js'
+        // filename: '[name].[hash].js'
+        filename: '[name].[chunkHash].js'
+    },
+
+    plugins:[
+        new Webpack.NamedChunksPlugin(),
         
-    },
+        new Webpack.NamedModulesPlugin(),
 
-    resolve: {
-        alias: {
-            jquery$: path.resolve(__dirname, 'src/libs/jquery.min.js') //$ 确定只是把jquery关键字解析到某一个目录的文件下，不是解析到某一个目录 确切匹配
-        }
-    },
-
-    module: {
-        rules: [
-            {
-                test:/\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['env']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextWebpackPlugin.extract(
-                    {
-                        fallback: {
-                            loader: 'style-loader',
-                            options: {
-                                singleton: true,
-                                // transform: './css.transform.js'
-                            }
-                        },
-                        use: [
-                            {
-                                loader: 'css-loader',
-                                options: {
-                                    importLoaders: 2
-                                    // minimize: true,
-                                    // modules: true,
-                                    // localIdentName: '[path][name]_[local]_[hash:base64:5]'
-                                }
-                                // loader: 'file-loader'
-                            },
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    ident: 'postcss',
-                                    plugins: [
-                                        require('postcss-sprites')({
-                                            spritePath: 'dist/assets/imgs/sprites',
-                                            retina: true
-                                        }),    
-                                        require('postcss-cssnext')()
-                                    ]
-                                }
-                            },
-                            {
-                                loader: 'less-loader'
-                            }
-                        ]
-                    })
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/,
-                use: [
-                    // {
-                    //     loader: 'file-loader',
-                    //     options: {
-                    //         // name: '[name]-[hash:5].[ext]',
-                    //         // limit: 1000,
-                    //         // publicPath: '',
-                    //         outputPath: 'imagesa',
-                    //         useRelativePath: true
-                    //     }
-                    // }
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            // name: '[name]-[hash:5].[ext]',
-                            /* name: '[name].min.[ext]',
-                            limit: 5000,
-                            publicPath: '',
-                            outputPath: 'dist/', // output publicPath
-                            useRelativePath: true */
-
-                            name: '[name].min.[ext]',
-                            limit: 1000,
-                            outputPath: 'assets/imgs/'
-                        }
-                    },
-                    {
-                        loader: 'img-loader',
-                        options: {
-                            pngquant: {
-                                quality: 80
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test:/\.(eot|woff2?|ttf|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name]-[hash:5].[ext]',
-                            limit: 5000,
-                            publicPath: '',
-                            outputPath: 'dist/',
-                            useRelativePath: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: path.resolve(__dirname, 'src/app.js'),
-                use: [
-                    {
-                        loader: 'imports-loader',
-                        options: {
-                            $: 'jquery'
-                        }
-                    }
-                ]
-            },
-            // {
-            //     test:/\.html$/,
-            //     use: [
-            //         {
-            //             loader: 'html-loader',
-            //             options: {
-            //                 attrs: ['img:src', 'img:data-src']
-            //             }
-            //         }
-            //     ]
-            // },
-        ]
-    },
-
-    plugins: [
-        // extractLess,
-        new ExtractTextWebpackPlugin({
-            filename: '[name]-bundle-[hash:5].css',
-            // filename: '[name].bundle.css',
-            allChunks: false
-        }),
-
-        // CSS Tree shaking
-        new PurifyCSS({
-            paths: glob.sync([
-                path.join(__dirname, './*.html'),
-                path.join(__dirname, './src/*.js')
-            ])
+        new Webpack.optimize.CommonsChunkPlugin({
+            name: ['vendor'],
+            minChunks: Infinity
         }),
 
         new Webpack.optimize.CommonsChunkPlugin({
             name: 'manifest'
-        }),
-
-        new HtmlInlinkChunkPlugin({
-            inlineChunks: ['manifest']
-        }),
-
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './index.html',
-            // chunks: ['app'],
-            minify: {
-                collapseWhitespace: true
-            }
-            // inject: false
-        }),
-
-        // new Webpack.ProvidePlugin({
-        //     $: 'jquery'
-        // }),
-
-        // JS Tree shaking
-        new Webpack.optimize.UglifyJsPlugin()
+        })
     ]
 }
