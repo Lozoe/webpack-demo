0adb81e0373f7b3f0db648a2314e78abb4b730bc
zhuo.liu <zhuo.liu@qunar.com>
2018-09-10 10:15:41 +0800

    feat: tree-shaking

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-console.log('This is index')
-export default 'This is index'
+// import 'preact'
+// import 'lodash'
+
+class Hello {
+  constructor (x, y) {
+    console.log('HELLO WORLD!')
+  }
+}
+
+import { add } from './modules/moduleA'
+add(1, 5)
+add(3, 4)
+
+let hello = new Hello()
+console.log(hello)
+
+// import(/* webpackChunkName: "moduleA" */'./modules/moduleA.js')
+import(/* webpackChunkName: "moduleB" */'./modules/moduleB.js')
+import(/* webpackChunkName: "moduleC" */'./modules/moduleC.js')
+import(/* webpackChunkName: "moduleD" */'./modules/moduleD.js')
---
+++ webpack.prod.config.js
@@ -0,0 +1,73 @@
+let HTMLWebpackPlugin = require('html-webpack-plugin')
+let path = require('path')
+const UglifyJs = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+
+    entry: {
+        'app': './src/index'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            inject: true,
+            template: path.resolve(__dirname, 'index.html')
+        })
+    ],
+
+    optimization: {
+        // minimize: false
+        minimizer: [
+            new UglifyJs({
+                uglifyOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true,
+
+                    ecma: 6,
+                    cache: true,
+                    parallel: true
+                }
+            })
+        ],
+
+        runtimeChunk: true, // true, false, single, multiple
+
+        // splitChunks: {
+        //     name: true,
+        //     minSize: 0,
+        //     cacheGroups: {
+        //         preact: {
+        //             test: /preact/,
+        //             chunks: 'initial'
+        //         },
+        //         lodash: {
+        //             test: /lodash/, 
+        //             chunks: 'all'
+        //         }
+        //     }
+        // }
+    }
+}
