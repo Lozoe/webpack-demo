64d9b40741e14237b0fad48eac5779a237b9fcaf
zhuo.liu <zhuo.liu@qunar.com>
2018-10-28 15:51:59 +0800

    feat: less and hot-reload

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,6 +11,12 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/base.less';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { AppContainer } from 'react-hot-loader';
+
+fetch('/api/comments/show?id=4199740256395164&page=1')
+  .then(res => {
+    // res.json().then(data => {
+      console.log(res)
+    // })
+  })
+// react-hot-loader
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if(module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  })
+}
